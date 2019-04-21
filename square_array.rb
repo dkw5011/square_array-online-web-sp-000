d99cf5651bd003af46a64974138455ea33c4c1e7
Danielle Bergbower <dkw5011@gmail.com>
2019-04-21 18:02:40 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,9 @@
+require 'pry'
 def square_array(array)
   array.each do |number|
   squared_number = (number * number)
   squared_array = []
   squared_array << (squared_number)
 end
+binding.pry
 end
