124dcdea67ae27c831dd745318c92bd4e9ae3d84
Danielle Bergbower <dkw5011@gmail.com>
2019-04-21 18:05:16 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,9 @@
 require 'pry'
 def square_array(array)
+  
   array.each do |number|
   squared_number = (number * number)
-  squared_array = []
+  
   squared_array << (squared_number)
   binding.pry
 end
